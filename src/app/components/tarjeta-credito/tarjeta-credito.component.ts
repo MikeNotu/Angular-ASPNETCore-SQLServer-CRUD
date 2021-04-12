9f29aa1b5272ca836051ac411a58eac13b23b27d
MikeNotu <mikebateman98@gmail.com>
2021-04-12 07:24:06 -0500

    List

--- src/app/components/tarjeta-credito/tarjeta-credito.component.ts
+++ src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tarjeta-credito.component.css']
 })
 export class TarjetaCreditoComponent implements OnInit {
-
+listTarjetas: any[] = [
+	{titular:'Juan Perez', numeroTarjeta:'1111222233334444', fechaExpiracion: '11/23', cvv:'123'},
+	{titular:'Miguel Gonzalez', numeroTarjeta:'5555222233334444', fechaExpiracion: '11/24', cvv:'321'},
+];
   constructor() { }
 
   ngOnInit(): void {
